5fb5cea73c2a5ac1b7452fc13dac86af242799e7
TheChunkyPumpkinhead <chriscampos@remainobscure.com>
2024-03-05 16:43:15 -0800

    adding css

--- nicepizza/src/index.js
+++ nicepizza/src/index.js
@@ -56,7 +56,8 @@ function App() {
   );
 }
 function Header() {
-  return <h1>Za Time</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return <h1 style={style}>Za Time</h1>;
 }
 
 function Menu() {
